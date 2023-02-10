e75ce253ab8a39fb704c1a6b8c01387005f49981
Jesse Alvarado <contact@jessealvarado.com>
2023-02-10 19:24:37 +0900

    refactor: relocate motion to root

--- features/layout/public/PublicLayout.tsx
+++ features/layout/public/PublicLayout.tsx
@@ -1,7 +1,6 @@
 import { PublicHeader } from './PublicHeader';
 import { PublicFooter } from './PublicFooter';
 import { Box } from '@mui/system';
-import { motion, AnimatePresence } from 'framer-motion';
 import { ReactNode } from 'react';
 
 export default function Layout({
@@ -17,28 +16,7 @@ export default function Layout({
       minHeight="100vh"
     >
       <PublicHeader title={children.props.title} />
-      <AnimatePresence>
-        <motion.div
-          initial="pageInitial"
-          animate="pageAnimate"
-          key={children?.props?.title}
-          variants={{
-            pageInitial: {
-              opacity: 0,
-            },
-            pageAnimate: {
-              opacity: 1,
-            },
-            pageExit: {
-              backgroundColor: 'white',
-              filter: `invert()`,
-              opacity: 0,
-            },
-          }}
-        >
-          <main style={{ display: 'contents' }}>{children}</main>
-        </motion.div>
-      </AnimatePresence>
+      <main style={{ display: 'contents' }}>{children}</main>
       <PublicFooter />
     </Box>
   );
