86989fdb3c5eb81a288744b9f6242af717aa0d0a
Jesse Alvarado <contact@jessealvarado.com>
2023-02-13 18:07:55 +0900

    refactor: remove version number and name for vercel

--- features/layout/public/PublicFooter.tsx
+++ features/layout/public/PublicFooter.tsx
@@ -1,13 +1,9 @@
-import { capitalize } from '@/helpers/textHelpers';
 import { Box } from '@mui/material';
-import VersionText from '../../../components/typography/VersionText';
 
 export function PublicFooter() {
   return (
     <Box textAlign="center" marginY={4}>
-      v<VersionText />
-      <br />
-      {capitalize(process.env.NEXT_PUBLIC_NAME)} © {new Date().getFullYear()}
+      Seasoning © {new Date().getFullYear()}
     </Box>
   );
 }
