28f354b7972c033b91088a5a3832595218ba5b64
Jesse Alvarado <contact@jessealvarado.com>
2023-06-20 13:51:51 +0900

    refactor: connect event chart to table selection

--- pages/showcase/charts/LineCharts3Example.tsx
+++ pages/showcase/charts/LineCharts3Example.tsx
@@ -9,6 +9,12 @@ import {
 } from 'recharts';
 import { Box } from '@mui/material';
 import { useEffect, useState } from 'react';
+import TableContainer from '@mui/material/TableContainer';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
 
 type ChartData = {
   date: string;
@@ -18,6 +24,7 @@ type ChartData = {
 
 export default function LineCharts3Example() {
   const [chartData, setChartData] = useState<ChartData[]>([]);
+  const [selectedPoint, setSelectedPoint] = useState<ChartData[]>([]);
 
   useEffect(() => {
     const data = [
@@ -39,6 +46,7 @@ export default function LineCharts3Example() {
     ];
 
     const developers = ['山田 太郎', '鈴木 花子', '田中 一郎'];
+
     const chartData = data
       .filter((item) => developers.includes(item.developer))
       .map((item) => ({
@@ -60,26 +68,65 @@ export default function LineCharts3Example() {
 
   const handleClick = (data, index) => {
     console.log(`You clicked on ${index}:`, data);
+    console.log('chartdata:', chartData);
+    setSelectedPoint(data?.activePayload);
   };
 
   return (
-    <Box sx={{ width: '800px', mx: 'auto' }}>
-      <LineChart
-        width={800}
-        height={400}
-        data={chartData}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        onClick={handleClick}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="山田 太郎" stroke="tomato" />
-        <Line type="monotone" dataKey="鈴木 花子" stroke="purple" />
-        <Line type="monotone" dataKey="田中 一郎" stroke="gold" />
-      </LineChart>
-    </Box>
+    <>
+      <Box sx={{ width: '800px', mx: 'auto' }}>
+        <LineChart
+          width={800}
+          height={400}
+          data={chartData}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          onClick={handleClick}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="山田 太郎" stroke="tomato" />
+          <Line type="monotone" dataKey="鈴木 花子" stroke="purple" />
+          <Line type="monotone" dataKey="田中 一郎" stroke="gold" />
+        </LineChart>
+      </Box>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Date</TableCell>
+              <TableCell>Developer</TableCell>
+              <TableCell>Merges</TableCell>
+              <TableCell>Notes</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {chartData.map((datapoint, index) => (
+              <TableRow
+                key={index}
+                sx={{
+                  backgroundColor: selectedPoint?.some(
+                    (selected) => selected?.payload?.date === datapoint.date
+                  )
+                    ? 'lightcyan'
+                    : 'white',
+                }}
+              >
+                <TableCell>
+                  Selected:{JSON.stringify(selectedPoint[0], null, 2)}
+                </TableCell>
+                <TableCell>
+                  Datapoint:{JSON.stringify(datapoint, null, 2)}
+                </TableCell>
+                <TableCell>{datapoint.merges}</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
