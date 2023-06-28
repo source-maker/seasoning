779b02dd0ec5a349f4014dcc26fe3ac83d719ef7
Jesse Alvarado <contact@jessealvarado.com>
2023-06-28 15:07:30 +0900

    refactor: sync swagger to BE

--- init/swagger/models/CategoryEnum.ts
+++ init/swagger/models/CategoryEnum.ts
@@ -2,6 +2,19 @@
 /* tslint:disable */
 /* eslint-disable */
 
+/**
+ * * `BEVERAGE` - BEVRAGE
+ * * `BAKERY` - BAKERY
+ * * `CANNED` - CANNED
+ * * `DAIRY` - DAIRY
+ * * `DRY` - DRY
+ * * `FROZEN` - FROZEN
+ * * `PRODUCE` - PRODUCE
+ * * `CLEANERS` - CLEANERS
+ * * `PAPER_GOODS` - PAPER_GOODS
+ * * `PERSONAL_CARE` - PERSONAL_CARE
+ * * `OTHER` - OTHER
+ */
 export enum CategoryEnum {
     BEVERAGE = 'BEVERAGE',
     BAKERY = 'BAKERY',
