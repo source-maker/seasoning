694ce043dc5a805d7ae26fac5f2ba5d0be12c697
Jesse Alvarado <contact@jessealvarado.com>
2023-07-06 18:16:22 +0900

    refactor: improve documentation for useSwagger hook

--- src/hooks/useSwagger.ts
+++ src/hooks/useSwagger.ts
@@ -2,9 +2,19 @@ import { SwaggerContext } from '@/providers/SwaggerProvider';
 import { useContext } from 'react';
 
 /**
- * Hook that provides access to the latest Swagger API client, which is potentially authenticated.
- * @returns {swaggerClient} A potentially authenticated Swagger API client.
- * @throws {Error} Will throw an error if used outside of a SwaggerProvider.
+ * `useSwagger` hook.
+ *
+ * This hook provides access to the Swagger API client within the context of the SwaggerProvider.
+ * It allows to interact with the Swagger API using the client object which might be authenticated depending on the user session.
+ *
+ * @return {Object} The returned object contains the `swaggerClient` which is an instance of Swagger API client.
+ * @throws {Error} Throws an error if the hook is being used outside the context of a SwaggerProvider.
+ *
+ * @example
+ * const { swaggerClient } = useSwagger();
+ * swaggerClient?.meRetrieve();
+ *
+ * @see {@link SwaggerProvider} for provider implementation details.
  */
 export const useSwagger = () => {
   const context = useContext(SwaggerContext);
