b14630cc8032175f5af978fbac494ebd3f185446
Jesse Alvarado <contact@jessealvarado.com>
2023-07-07 14:04:34 +0900

    feat: add string to JSX utility function

--- src/helpers/stringHelpers.tsx
+++ src/helpers/stringHelpers.tsx
@@ -10,3 +10,16 @@ export function limitLength(string = '', limit = 0) {
 export function capitalize(string = '') {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+// convert translation json to line break string
+export function convertStringToJSX(text) {
+  // Split the text by line break, then map each line to a JSX element
+  const lines = text.split('\n').map((line, index) => (
+    <span key={index}>
+      {line}
+      <br />
+    </span>
+  ));
+
+  return <>{lines}</>;
+}
