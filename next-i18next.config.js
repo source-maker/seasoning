53ca2327213253ef149d22986dc9b1461fa8d612
Jesse Alvarado <contact@jessealvarado.com>
2023-07-11 21:09:28 +0900

    refactor: cleanup useless comments

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   i18n: {
-    // all the locales supported in the application
-    locales: ['en', 'ja'],
-    // default for a non-localized route (e.g. `/about`)
     defaultLocale: 'ja',
+    locales: ['en', 'ja'],
   },
 };
